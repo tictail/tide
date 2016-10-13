2e3005f88fa930e2a896d89c48cf57af0f7d643f
eldh <andreas.eldh@gmail.com>
2016-10-13 12:34:47 +0200

    Add script

--- release.sh
+++ release.sh
@@ -5,8 +5,6 @@ set -e
 VERSION=`grep "version" package.json | cut -d '"' -f 4`
 GIT_CMD="git -c user.name=\"CircleCI\" -c user.email=\"tech+circleci@tictail.com\""
 
-npm pack
-
 echo "Creating git tag ${VERSION}"
 set +e
 ${GIT_CMD} tag -a ${VERSION} -m ${VERSION} HEAD
@@ -14,11 +12,11 @@ GIT_RET_CODE=$?
 set -e
 
 if [ ${GIT_RET_CODE} -eq 0 ]; then
-    curl --fail -F package=@`ls -1 tictail-*.tgz | sort -r | head -1` ${GEMFURY_INDEX}
+    echo "Publishing ${VERSION} to npm"
+    echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
+    npm run 2npm
     echo "Pushing git tag ${VERSION}"
     ${GIT_CMD} push --tags
 else
-    echo "Creating git tag failed, most likely due to tag already existing - upload to Gemfury skipped"
+    echo "Creating git tag failed, most likely due to tag already existing"
 fi
-
-rm -rf tictail-tide*.tgz
